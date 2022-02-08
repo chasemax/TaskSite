5df545fb0c8de983ae9fc6cfba8ec9dd70c8622f
EzraMarin <90418312+EzraMarin@users.noreply.github.com>
2022-02-08 12:15:34 -0700

    category names

--- Models/TaskContext.cs
+++ Models/TaskContext.cs
@@ -17,6 +17,14 @@ namespace TaskSite.Models
         public DbSet <TaskInfo> TaskInfo { get; set; }
         public DbSet <Category> Categories { get; set; }
 
-
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                    new Category { CategoryId = 1, CategoryName = "Home" },
+                    new Category { CategoryId = 2, CategoryName = "School" },
+                    new Category { CategoryId = 3, CategoryName = "Work" },
+                    new Category { CategoryId = 4, CategoryName = "Church" }
+                );
+        }
     }
 }
